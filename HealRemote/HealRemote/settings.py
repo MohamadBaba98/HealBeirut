aca7af78374c4e8e4b6de905f510177d8cc2069b
Kyle Smurlo <smurflo2@gmail.com>
2021-09-30 15:49:52 -0500

    Set up basic index page for scheduling

--- HealRemote/HealRemote/settings.py
+++ HealRemote/HealRemote/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'scheduling.apps.SchedulingConfig',
 ]
 
 MIDDLEWARE = [
