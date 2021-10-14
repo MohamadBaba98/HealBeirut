b486894733f13102b3078088ffcc8657b507400c
Kyle Smurlo <smurflo2@gmail.com>
2021-10-14 15:00:08 -0500

    Starting point on Scheduling models

--- HealRemote/scheduling/models.py
+++ HealRemote/scheduling/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Appointment(models.Model):
+    provider = models.ForeignKey(User, on_delete=models.CASCADE, related_name='provider')
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient')
+
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    reason_for_visit = models.CharField(max_length=500, default='')
+
+
+class Availability(models.Model):
+    provider = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # How do we want to store availability? Depends on UI
+    #   Days of week x hours for those days?
+    #   Exceptions - list of dates(/times?) when unavailable for things like vacation or whatever
+    #   start_date + end_date (inclusive)
+    #  or maybe start_date, end_date, start_time, end_time, dates_available)
+
+    # preferences?
+    #   duration, time_between_appointments
+
+
+
