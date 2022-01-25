c4ad2430046d2ebb6ad0c9c200ff17f34f60a970
packerfan626 <dori_mouawad@hotmail.com>
2022-01-24 23:07:28 -0800

    Added initial splash screen code

--- HealRemote/mobile/ios/mobile/AppDelegate.m
+++ HealRemote/mobile/ios/mobile/AppDelegate.m
@@ -29,7 +29,7 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"MySplashScreen"
+                                                   moduleName:@"HealBeirut"
                                             initialProperties:nil];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
